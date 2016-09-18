9c3f0322402da5436700e910109522143ba6ef98
Gregor <gregor@martynus.net>
2016-09-18 15:35:45 -0700

    feat(teams): Code Team landing page

--- app/routes/teams.js
+++ app/routes/teams.js
@@ -1,12 +1,41 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  github: Ember.inject.service('github'),
   model(params) {
-    return {
-      name: params.name
-    };
+    const teamLabel = {
+      code: 'Code'
+    }[params.name];
+
+    return this.get('github').getCampIssues({
+      labels: [teamLabel, 'up for grabs']
+    })
+
+    .then(function (issues) {
+      return {
+        name: params.name,
+        yourFirstPRIssues: hasLabel(issues, 'Your First PR'),
+        notYourFirstPRIssues: hasntLabel(issues, 'Your First PR')
+      };
+    });
   },
   renderTemplate(controller, model) {
-    this.render(`teams/${model.name}`);
+    this.render(`teams/${model.name}`, {model});
   }
 });
+
+function hasLabel(issues, label) {
+  return issues.filter((issue) => {
+    return issue.labels.filter((issueLabel) => {
+      return issueLabel.name === label;
+    }).length === 1;
+  });
+}
+
+function hasntLabel(issues, label) {
+  return issues.filter((issue) => {
+    return issue.labels.filter((issueLabel) => {
+      return issueLabel.name === label;
+    }).length === 0;
+  });
+}
